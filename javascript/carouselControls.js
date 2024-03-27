27364bbe42b07e4a310e054dbcaf8590e882d1da
marcelonogjr <97480299+marcelonogjr@users.noreply.github.com>
2024-03-27 15:33:19 -0300

    second step improvement

---
+++ javascript/carouselControls.js
@@ -0,0 +1,31 @@
+export function carouselControls(elementId) {
+  const multipleCardCarousel = document.querySelector(elementId);
+  if (window.matchMedia('(min-width: 768px)').matches) {
+    const carousel = new bootstrap.Carousel(multipleCardCarousel, {
+      interval: false,
+    });
+    const carouselWidth = $('.carousel-inner-card')[0].scrollWidth;
+    const cardWidth = $('.carousel-item-card').width();
+    let scrollPosition = 0;
+    $(`${elementId} > .carousel-control-next`).on('click', function () {
+      if (scrollPosition < carouselWidth - cardWidth * 4) {
+        scrollPosition += cardWidth;
+        $(`${elementId} > .carousel-inner-card`).animate(
+          { scrollLeft: scrollPosition },
+          600
+        );
+      }
+    });
+    $(`${elementId} > .carousel-control-prev`).on('click', function () {
+      if (scrollPosition > 0) {
+        scrollPosition -= cardWidth;
+        $(`${elementId} > .carousel-inner-card`).animate(
+          { scrollLeft: scrollPosition },
+          600
+        );
+      }
+    });
+  } else {
+    $(multipleCardCarousel).addClass('slide');
+  }
+}
--- javascript/main.js
+++ javascript/main.js
@@ -1,36 +1,5 @@
 import '../css/style.css';
-
-function carouselControls(elementId) {
-  const multipleCardCarousel = document.querySelector(elementId);
-  if (window.matchMedia('(min-width: 768px)').matches) {
-    const carousel = new bootstrap.Carousel(multipleCardCarousel, {
-      interval: false,
-    });
-    const carouselWidth = $('.carousel-inner-card')[0].scrollWidth;
-    const cardWidth = $('.carousel-item-card').width();
-    let scrollPosition = 0;
-    $(`${elementId} > .carousel-control-next`).on('click', function () {
-      if (scrollPosition < carouselWidth - cardWidth * 4) {
-        scrollPosition += cardWidth;
-        $(`${elementId} > .carousel-inner-card`).animate(
-          { scrollLeft: scrollPosition },
-          600
-        );
-      }
-    });
-    $(`${elementId} > .carousel-control-prev`).on('click', function () {
-      if (scrollPosition > 0) {
-        scrollPosition -= cardWidth;
-        $(`${elementId} > .carousel-inner-card`).animate(
-          { scrollLeft: scrollPosition },
-          600
-        );
-      }
-    });
-  } else {
-    $(multipleCardCarousel).addClass('slide');
-  }
-}
+import { carouselControls } from './carouselControls';
 
 carouselControls('#carouselControls');
 carouselControls('#carouselControls2');
