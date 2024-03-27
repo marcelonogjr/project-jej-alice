2feb47bc9cdf3f292204520465b01e0cd6edf249
marcelonogjr <97480299+marcelonogjr@users.noreply.github.com>
2024-03-27 15:31:26 -0300

    first step improvement and carousel image link fix

--- javascript/main.js
+++ javascript/main.js
@@ -1,66 +1,36 @@
-import '../css/style.css'
+import '../css/style.css';
 
-var multipleCardCarousel = document.querySelector(
-  "#carouselControls"
-);
-if (window.matchMedia("(min-width: 768px)").matches) {
-  var carousel = new bootstrap.Carousel(multipleCardCarousel, {
-    interval: false,
-  });
-  var carouselWidth = $(".carousel-inner-card")[0].scrollWidth;
-  var cardWidth = $(".carousel-item-card").width();
-  var scrollPosition = 0;
-  $("#carouselControls > .carousel-control-next").on("click", function () {
-    if (scrollPosition < carouselWidth - cardWidth * 4) {
-      scrollPosition += cardWidth;
-      $("#carouselControls > .carousel-inner-card").animate(
-        { scrollLeft: scrollPosition },
-        600
-      );
-    }
-  });
-  $("#carouselControls > .carousel-control-prev").on("click", function () {
-    if (scrollPosition > 0) {
-      scrollPosition -= cardWidth;
-      $("#carouselControls > .carousel-inner-card").animate(
-        { scrollLeft: scrollPosition },
-        600
-      );
-    }
-  });
-} else {
-  $(multipleCardCarousel).addClass("slide");
+function carouselControls(elementId) {
+  const multipleCardCarousel = document.querySelector(elementId);
+  if (window.matchMedia('(min-width: 768px)').matches) {
+    const carousel = new bootstrap.Carousel(multipleCardCarousel, {
+      interval: false,
+    });
+    const carouselWidth = $('.carousel-inner-card')[0].scrollWidth;
+    const cardWidth = $('.carousel-item-card').width();
+    let scrollPosition = 0;
+    $(`${elementId} > .carousel-control-next`).on('click', function () {
+      if (scrollPosition < carouselWidth - cardWidth * 4) {
+        scrollPosition += cardWidth;
+        $(`${elementId} > .carousel-inner-card`).animate(
+          { scrollLeft: scrollPosition },
+          600
+        );
+      }
+    });
+    $(`${elementId} > .carousel-control-prev`).on('click', function () {
+      if (scrollPosition > 0) {
+        scrollPosition -= cardWidth;
+        $(`${elementId} > .carousel-inner-card`).animate(
+          { scrollLeft: scrollPosition },
+          600
+        );
+      }
+    });
+  } else {
+    $(multipleCardCarousel).addClass('slide');
+  }
 }
 
-// male carousel
-var multipleCardCarousel2 = document.querySelector(
-  "#carouselControls2"
-);
-if (window.matchMedia("(min-width: 768px)").matches) {
-  var carousel = new bootstrap.Carousel(multipleCardCarousel2, {
-    interval: false,
-  });
-  var carouselWidth2 = $(".carousel-inner-card")[0].scrollWidth;
-  var cardWidth2 = $(".carousel-item-card").width();
-  var scrollPosition2 = 0;
-  $("#carouselControls2 > .carousel-control-next").on("click", function () {
-    if (scrollPosition2 < carouselWidth2 - cardWidth2 * 4) {
-      scrollPosition2 += cardWidth2;
-      $("#carouselControls2>.carousel-inner-card").animate(
-        { scrollLeft: scrollPosition2 },
-        600
-      );
-    }
-  });
-  $("#carouselControls2 > .carousel-control-prev").on("click", function () {
-    if (scrollPosition2 > 0) {
-      scrollPosition2 -= cardWidth2;
-      $("#carouselControls2 > .carousel-inner-card").animate(
-        { scrollLeft: scrollPosition2 },
-        600
-      );
-    }
-  });
-} else {
-  $(multipleCardCarousel2).addClass("slide");
-}
+carouselControls('#carouselControls');
+carouselControls('#carouselControls2');
